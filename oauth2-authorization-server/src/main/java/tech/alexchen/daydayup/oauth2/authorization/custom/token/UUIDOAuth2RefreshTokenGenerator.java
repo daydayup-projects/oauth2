62822ecc6b4aa9aba5d9d63c3b38b46b66e3a84d
alexchen <1274812218@qq.com>
2023-12-03 15:21:58 +0800

    style: 优化格式

--- oauth2-authorization-server/src/main/java/tech/alexchen/daydayup/oauth2/authorization/custom/token/UUIDOAuth2RefreshTokenGenerator.java
+++ oauth2-authorization-server/src/main/java/tech/alexchen/daydayup/oauth2/authorization/custom/token/UUIDOAuth2RefreshTokenGenerator.java
@@ -4,9 +4,9 @@ import org.springframework.lang.Nullable;
 import org.springframework.security.crypto.keygen.StringKeyGenerator;
 import org.springframework.security.oauth2.core.OAuth2RefreshToken;
 import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
+import org.springframework.security.oauth2.server.authorization.token.OAuth2RefreshTokenGenerator;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenGenerator;
-import org.springframework.security.oauth2.server.authorization.token.OAuth2RefreshTokenGenerator;
 
 import java.time.Instant;
 
--- oauth2-resource-server/src/main/java/tech/alexchen/daydayup/oauth2/resource/ResourceServerApplication.java
+++ oauth2-resource-server/src/main/java/tech/alexchen/daydayup/oauth2/resource/ResourceServerApplication.java
@@ -2,10 +2,7 @@ package tech.alexchen.daydayup.oauth2.resource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
 @EnableMethodSecurity
 @SpringBootApplication
